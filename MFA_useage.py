528579e3641b5a2cceba11f6f12dcce521baaed2
James le Roux <jamesleroux@Jamess-MBP.fritz.box>
2017-05-28 23:29:45 +0200

    MFA_useage.py git commit -m MFA_useage.py
    exit

---
+++ MFA_useage.py
@@ -0,0 +1,17 @@
+%matplotlib inline
+
+import os
+
+import IPython.display as ipd
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn as skl
+import sklearn.utils, sklearn.preprocessing, sklearn.decomposition, sklearn.svm
+import librosa
+import librosa.display
+
+import utils
+
+plt.rcParams['figure.figsize'] = (17, 5)
